895879ebce6498e93b4f2072b0197a5ab009182c
James Losole <jameslosole@dhcp-10-142-181-7.uawifi.arizona.edu>
2016-11-21 15:17:15 -0700

    added controller package and game.java

--- src/controller/MapGUI.java
+++ src/controller/MapGUI.java
@@ -1,4 +1,4 @@
-package model;
+package controller;
 
 import java.awt.BorderLayout;
 import java.awt.Point;
---
+++ src/controller/MyArrowKeyListener.java
@@ -0,0 +1,33 @@
+package controller;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import model.Game;
+
+public class MyArrowKeyListener implements KeyListener {
+	
+	private Game game;
+
+	public MyArrowKeyListener(Game game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
