a3968b6771b598b1fcbef2665ace600a08c51e2b
Cullen Bottock <Cbottock@Cullens-MacBook-Pro.local>
2016-12-05 17:23:08 -0700

    Updated InventoryView

--- src/views/InventoryView.java
+++ src/views/InventoryView.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
@@ -20,7 +21,7 @@ import model.Pokemon.PokemonType;
 public class InventoryView extends JPanel implements Observer {
 	
 	private Game theGame;
-	private int width, height;
+	private int width, height, spacing = 20;
 	private HashMap<PokemonType, Integer> pokemonList;
 	private HashMap<ItemType, Integer> itemList;
 	private BufferedImage textBox;
@@ -52,7 +53,7 @@ public class InventoryView extends JPanel implements Observer {
 		itemLabel.setFont(new Font("Courier", Font.BOLD, 24));
 		itemLabel.setText("Items");
 		itemLabel.setSize(100, 20);
-		itemLabel.setLocation((width/4)*3, 100);
+		itemLabel.setLocation((width/4)*3-100, 100);
 	}
 
 	@Override
@@ -61,6 +62,11 @@ public class InventoryView extends JPanel implements Observer {
 	}
 	
 	public void paintComponent(Graphics g){
+		spacing = 40;
+		
+		//Get updated hash maps
+		pokemonList = theGame.getTrainer().getPokemon();
+		itemList = theGame.getTrainer().getItems();
 		
 		//Draw the pokemon text box
 		g.drawImage(textBox, 0, 0, null);
@@ -68,5 +74,30 @@ public class InventoryView extends JPanel implements Observer {
 		//Add the labels
 		this.add(pokemonLabel);
 		this.add(itemLabel);
+		
+		//Setup font
+		g.setFont(new Font("Courier", Font.BOLD, 16));
+		
+		//Draw pokemon list
+		Set<PokemonType> pokemonSet = pokemonList.keySet();
+		for(PokemonType pokemon : pokemonSet){
+			int numPokemon = pokemonList.get(pokemon);
+			for(int i = 0; i < numPokemon; i++) {
+				g.drawString(pokemon.toString(), width/7, 100+spacing);
+				spacing += 20;
+			}
+		}
+		
+		//Reset spacing for y-axis 
+		spacing = 40;
+		
+		//Draw items list
+		Set<ItemType> itemSet = itemList.keySet();
+		for(ItemType item : itemSet){
+			int numItem = itemList.get(item);
+			String numItemAsString = Integer.toString(numItem);
+			g.drawString(item.toString() + " x " + numItemAsString, (width/4)*3-100, 100+spacing);
+			spacing += 20;
+		}
 	}
 }
